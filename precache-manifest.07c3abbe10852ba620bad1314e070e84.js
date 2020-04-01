05699a5d64fee258c5dc0bf1414767e7d7f666fe
jeevanathg <jeevanath.g@gmail.com>
2020-04-02 05:01:10 +0530

    Updates

---
+++ precache-manifest.07c3abbe10852ba620bad1314e070e84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca9b0d1218829990c182d92081d0f03",
+    "url": "/react-redux-ecommerce/index.html"
+  },
+  {
+    "revision": "e5b559ffbf2aad1f4a63",
+    "url": "/react-redux-ecommerce/static/css/main.91bb8e84.chunk.css"
+  },
+  {
+    "revision": "6c55a34d83db61ad2cc5",
+    "url": "/react-redux-ecommerce/static/js/2.f6119f0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-redux-ecommerce/static/js/2.f6119f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b559ffbf2aad1f4a63",
+    "url": "/react-redux-ecommerce/static/js/main.29fdfecc.chunk.js"
+  },
+  {
+    "revision": "bef1a3a196e7859f0b1a",
+    "url": "/react-redux-ecommerce/static/js/runtime-main.d7efe2b2.js"
+  }
+]);
